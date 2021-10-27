469a4cb0ac2b150d31a07eacf7667a1290270550
markm5 <mark.mckenna@wrapmate.com>
2021-10-27 17:14:13 -0600

    big additions, added my whole game

---
+++ dump.js
@@ -0,0 +1,223 @@
+// kaboom({
+//     global: true,
+//     fullscreen: true,
+//     scale: 1,
+//     debug: true,
+//     clearColor: [0, 0, 0, 1],
+//   })
+  
+//   loadRoot("https://i.imgur.com/")
+//   loadSprite("coin", 'wbKxhcd.png')
+//   loadSprite("goomba", "KPO3fR9.png")
+//   loadSprite("brick", "pogC9x5.png")
+//   loadSprite("block", "M6rwarW.png")
+//   loadSprite("mario", "Wb1qfhK.png")
+//   loadSprite("shroom", "0wMd92p.png")
+//   loadSprite("boxClosed", "gesQ1KP.png")
+//   loadSprite("boxOpened", "bdrLpi6.png")
+//   loadSprite("pipeTopLeft", "ReTPiWY.png")
+//   loadSprite("pipeTopRight", "hj2GK4n.png")
+//   loadSprite("pipeBotLeft", "c1cYSbt.png")
+//   loadSprite("pipeBotRight", "nqQ79eI.png")
+//   loadSprite("blueBlock", "fVscIbn.png")
+//   loadSprite("blueBrick", "3e5YRQd.png")
+//   loadSprite("blueSteel", "gqVoI2b.png")
+//   loadSprite("blueGoomba", "SvV4ueD.png")
+//   loadSprite("blueBoxClosed", "RMqCc1G.png")
+  
+//   scene("game", ({ level, score }) => {
+//     layer(["bg", "obj", "ui"], "obj")
+//     const maps = [
+//       [
+//       "                                                        ",
+//       "                                                        ",
+//       "                                                        ",
+//       "                                                        ",
+//       "                                                        ",
+//       "         %   =*=%=                                      ",
+//       "                                                        ",
+//       "                                                        ",
+//       "                                  -+                    ",
+//       "                           ^  ^   ()                    ",
+//       "====================================   ================="
+//     ],
+//     [
+//       "e                                                        e",
+//       "e                                                        e",
+//       "e                                                        e",
+//       "e                                              -+        e",
+//       "e                                              ()        e",
+//       "e                                              xx        e",
+//       "e        @@@@@@@@                             xxx        e",
+//       "e                                            xxxx        e",
+//       "e                                  x     x  xxxxx        e",
+//       "e                       z    z     x     x xxxxxx        e",
+//       "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    !!!!!!!!!!!!!!!!!"
+//     ],
+//   ]
+  
+//     const levelConfig = {
+//       width: 20,
+//       height: 20,
+//       "=": [sprite("block"), solid()],
+//       "$": [sprite("coin")],
+//       "%": [sprite("boxClosed"), solid(), "coin-box"],
+//       "*": [sprite("boxClosed"), solid(), "shroom-box"],
+//       "}": [sprite("boxOpened"), solid()],
+//       "(": [sprite("pipeBotLeft"), solid(), scale(0.5)],
+//       ")": [sprite("pipeBotRight"), solid(), scale(0.5)],
+//       "-": [sprite("pipeTopLeft"), solid(), scale(0.5), "pipe"],
+//       "+": [sprite("pipeTopRight"), solid(), scale(0.5), "pipe"],
+//       "^": [sprite("goomba"), solid(), "danger"],
+//       "!": [sprite("blueBlock"), solid(), scale(0.5)],
+//       "e": [sprite("blueBrick"), solid(), scale(0.5)],
+//       "z": [sprite("blueGoomba"), solid(), scale(0.5), "danger"],
+//       "@": [sprite("blueBoxClosed"), solid(), scale(0.5), "coin-box"],
+//       "x": [sprite("blueSteel"), solid(), scale(0.5)],
+//     }
+  
+//     const gameLevel = addLevel(maps[level], levelConfig)
+  
+//     const MOVE_SPEED = 120;
+//     const SMALL_JUMP_FORCE = 360;
+//     const BIG_JUMP_FORCE = 550
+//     let CURRENT_JUMP_FORCE = SMALL_JUMP_FORCE;
+//     const ENEMY_SPEED = 50;
+//     let isJumping = true;
+//     const FALL_DEATH = 400;
+  
+//   const scoreLabel = add([
+//       text(score),
+//       pos(30, 6),
+//       layer("ui"),
+//       {
+//         value: score,
+//       }
+//     ])
+  
+//     add([text("level " + parseInt(level + 1)), pos(100,6)])
+  
+//     function big() {
+//       let timer = 0;
+//       let isBig = false
+//         return {
+//           update() {
+//             if (isBig) {
+//               timer -= dt()
+//               if (timer <= 0) {
+//                 this.smallify()
+//               }
+//             }
+//           },
+//           isBig() {
+//             return isBig
+//           },
+//           smallify() {
+//             this.scale = vec2(1)
+//             CURRENT_JUMP_FORCE = SMALL_JUMP_FORCE
+//             timer = 0
+//             isBig = false
+//           },
+//           biggify(time) {
+//             this.scale = vec2(2)
+//             CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
+//             timer = time
+//             isBig = true
+//           }
+//         }
+//     }
+  
+//     const player = add([
+//       sprite("mario"), solid(),
+//       //player position
+//       pos(30, 0),
+//       //adding gravity
+//       body(),
+//       origin("bot"),
+//       big()
+//     ])
+  
+//     action("shroom", (shroom) => {
+//       shroom.move(50, 0)
+//     })
+  
+//     player.on("headbump", (obj) => {
+//       if (obj.is("coin-box")) {
+//         gameLevel.spawn("$", obj.gridPos.sub(0, 1))
+//         destroy(obj)
+//         gameLevel.spawn("}", obj.gridPos.sub(0, 0))
+//       }
+//       if (obj.is("shroom-box")) {
+//         gameLevel.spawn("#", obj.gridPos.sub(0, 1))
+//         destroy(obj)
+//         gameLevel.spawn("}", obj.gridPos.sub(0, 0))
+//       }
+//     })
+  
+//     player.collides("shroom", (shroom) => {
+//       destroy(shroom)
+//       player.biggify(6)
+//     })
+  
+//     player.collides("coin", (coin) => {
+//       destroy(coin)
+//       scoreLabel.value++
+//       scoreLabel.text = scoreLabel.value
+//     })
+  
+//     action("danger", (danger) => {
+//       danger.move(-ENEMY_SPEED, 0)
+//     })
+  
+//     player.collides("danger", (danger) => {
+//       if(isJumping) {
+//         destroy(danger)
+//       } else {
+//         go("lose", {score: scoreLabel.value})
+//       }
+//     })
+//     player.collides("pipe", () => {
+//       keyPress("down", () => {
+//         go("game", {
+//           level: (level + 1),
+//           score: scoreLabel.value
+//         })
+//       })
+//     })
+    
+//     player.action(() => {
+//       camPos(player.pos)
+//       if (player.pos.y >= FALL_DEATH) {
+//         go("lose", {score: scoreLabel.value })
+//       }
+//     })
+  
+//     //key bindings
+  
+//     keyDown("left", () => {
+//       player.move(-MOVE_SPEED, 0)
+//     })
+//     keyDown("right", () => {
+//       player.move(MOVE_SPEED, 0)
+//     })
+  
+//     player.action(() => {
+//       if (player.grounded()) {
+//         isJumping = false;
+//       }
+//     })
+  
+//     keyPress("space", () => {
+//       if (player.grounded()) {
+//         isJumping = true
+//         player.jump(CURRENT_JUMP_FORCE)
+//       }
+//     })
+  
+//   })
+  
+//   scene("lose", ({ score }) => {
+//     add(text(score, 32), origin("center", pos(width()/2), height()/2))
+//   })
+  
+//   start("game", { level: 0, score: 0 })
--- game.js
+++ game.js
@@ -1,90 +1,119 @@
 kaboom({
   global: true,
   fullscreen: true,
-  scale: 1,
+  scale: 2,
   debug: true,
   clearColor: [0, 0, 0, 1],
 })
 
 loadRoot("https://i.imgur.com/")
-loadSprite("coin", 'wbKxhcd.png')
-loadSprite("goomba", "KPO3fR9.png")
-loadSprite("brick", "pogC9x5.png")
-loadSprite("block", "M6rwarW.png")
-loadSprite("mario", "Wb1qfhK.png")
-loadSprite("shroom", "0wMd92p.png")
-loadSprite("boxClosed", "gesQ1KP.png")
-loadSprite("boxOpened", "bdrLpi6.png")
-loadSprite("pipeTopLeft", "ReTPiWY.png")
-loadSprite("pipeTopRight", "hj2GK4n.png")
-loadSprite("pipeBotLeft", "c1cYSbt.png")
-loadSprite("pipeBotRight", "nqQ79eI.png")
-loadSprite("blueBlock", "fVscIbn.png")
-loadSprite("blueBrick", "3e5YRQd.png")
-loadSprite("blueSteel", "gqVoI2b.png")
-loadSprite("blueGoomba", "SvV4ueD.png")
-loadSprite("blueBoxClosed", "RMqCc1G.png")
+loadSprite("tile", "deQWSwM.gif")
+loadSprite("rock", "asp7asB.gif")
+loadSprite("wall", "yt9J5aB.gif")
+loadSprite("block", "Dtkl5lq.gif")
+loadSprite("snake", "XfjidQ6.gif")
+loadSprite("head", "0BcmJms.gif")
+loadSprite("boss1", "fZbyndX.gif")
+loadSprite("chestClosed", "nLWfuCu.png")
+loadSprite("chestOpened", "xySGRLI.png")
+loadSprite("sword", "KSdpQlu.gif")
+loadSprite("hook", "NkYuIQY.png")
+loadSprite("rope", "WuRDx20.png")
+loadSprite("player", "DEjwv7z.gif")
+loadSprite("exit", "7APcRJX.gif")
 
 scene("game", ({ level, score }) => {
   layer(["bg", "obj", "ui"], "obj")
   const maps = [
     [
-    "                                                        ",
-    "                                                        ",
-    "                                                        ",
-    "                                                        ",
-    "                                                        ",
-    "         %   =*=%=                                      ",
-    "                                                        ",
-    "                                                        ",
-    "                                  -+                    ",
-    "                           ^  ^   ()                    ",
-    "====================================   ================="
+    "%========================================================%",
+    "%                                                        %",
+    "%                                                        %",
+    "%                                                        %",
+    "%                                                    x   %",
+    "%                                                        %",
+    "%                                                gggggg  %",
+    "%                                      ggggggg           %",
+    "%                                                        %",
+    "%                                                        %",
+    "%                                 gggggg                 %",
+    "%                                                        %",
+    "%                         ggggggg                        %",
+    "%                                                        %",
+    "%                  ggggggg                               %",
+    "%                                                        %",
+    "%            ggggggg                                     %",
+    "%                                                        %",
+    "%                                                        %",
+    "%                                                        %",
+    "%                 ggggggg                                %",
+    "%                                                        %",
+    "%                                                        %",
+    "%                                                        %",
+    "%            gggggg                                      %",
+    "%                                              *         %",
+    "%                                             **         %",
+    "%                                            ***         %",
+    "%                      }                     ****      - %",
+    "=========================================================="
   ],
   [
-    "e                                                        e",
-    "e                                                        e",
-    "e                                                        e",
-    "e                                              -+        e",
-    "e                                              ()        e",
-    "e                                              xx        e",
-    "e        @@@@@@@@                             xxx        e",
-    "e                                            xxxx        e",
-    "e                                  x     x  xxxxx        e",
-    "e                       z    z     x     x xxxxxx        e",
-    "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!    !!!!!!!!!!!!!!!!!"
+    "%                    ggggggggggggg              %",
+    "%                                               %",
+    "%          ggggggg                              %",
+    "%                                               %",
+    "%                                               %",
+    "% gggggg                                        %",
+    "%                                               %",
+    "%                                               %",
+    "%       ggggggg                                 %",
+    "%                                               %",
+    "%        ggggggg                                %",
+    "%                                               %",
+    "%                                              x%",
+    "% ggggggg                             gggggggggg%",
+    "%                                               %",
+    "%                                               %",
+    "%                                               %",
+    "%       gggggg                                  %",
+    "%                                               %",
+    "%                            )                   %",
+    "%                                               %",
+    "%            -                                  %",
+    "================================================="
   ],
 ]
 
   const levelConfig = {
     width: 20,
     height: 20,
-    "=": [sprite("block"), solid()],
-    "$": [sprite("coin")],
-    "%": [sprite("boxClosed"), solid(), "coin-box"],
-    "*": [sprite("boxClosed"), solid(), "shroom-box"],
-    "}": [sprite("boxOpened"), solid()],
-    "(": [sprite("pipeBotLeft"), solid(), scale(0.5)],
-    ")": [sprite("pipeBotRight"), solid(), scale(0.5)],
-    "-": [sprite("pipeTopLeft"), solid(), scale(0.5), "pipe"],
-    "+": [sprite("pipeTopRight"), solid(), scale(0.5), "pipe"],
-    "^": [sprite("goomba"), solid(), "danger"],
-    "!": [sprite("blueBlock"), solid(), scale(0.5)],
-    "e": [sprite("blueBrick"), solid(), scale(0.5)],
-    "z": [sprite("blueGoomba"), solid(), scale(0.5), "danger"],
-    "@": [sprite("blueBoxClosed"), solid(), scale(0.5), "coin-box"],
-    "x": [sprite("blueSteel"), solid(), scale(0.5)],
+    "=": [sprite("tile"), solid(), scale(.65)],
+    "$": [sprite("rock"), solid()],
+    "g": [sprite("rock"), solid(), scale(.8), "grapple"],
+    "%": [sprite("wall"), solid()],
+    "*": [sprite("block"), solid(), "grapple"],
+    "}": [sprite("snake"), solid(), scale(.85),"danger"],
+    "(": [sprite("head"), solid(), "danger"],
+    ")": [sprite("boss1"), solid(), "danger"],
+    "-": [sprite("chestClosed"), solid(), scale(.1), "hookChest"],
+    "&": [sprite("chestClosed"), solid(), scale(.1), "swordChest"],
+    "+": [sprite("chestOpened"), solid(), scale(.1)],
+    "^": [sprite("sword"), solid()],
+    "!": [sprite("hook"), solid(), "hook", scale(.05)],
+    "e": [sprite("rope"), solid(), scale(.1), pos(0, 2)],
+    "x": [sprite("exit"), solid(), scale(1.5), "exit"],
   }
 
   const gameLevel = addLevel(maps[level], levelConfig)
 
   const MOVE_SPEED = 120;
-  const SMALL_JUMP_FORCE = 360;
-  const BIG_JUMP_FORCE = 550
-  let CURRENT_JUMP_FORCE = SMALL_JUMP_FORCE;
+  const JUMP_FORCE = 300;
   const ENEMY_SPEED = 50;
   let isJumping = true;
-  const FALL_DEATH = 400;
+  const FALL_DEATH = 600;
+  let hasHook = false;
+  let hasSword = false;
+  let direction = "right";
 
 const scoreLabel = add([
     text(score),
@@ -97,92 +126,53 @@ const scoreLabel = add([
 
   add([text("level " + parseInt(level + 1)), pos(100,6)])
 
-  function big() {
-    let timer = 0;
-    let isBig = false
+  function sword() {
       return {
-        update() {
-          if (isBig) {
-            timer -= dt()
-            if (timer <= 0) {
-              this.smallify()
-            }
-          }
-        },
-        isBig() {
-          return isBig
-        },
-        smallify() {
-          this.scale = vec2(1)
-          CURRENT_JUMP_FORCE = SMALL_JUMP_FORCE
-          timer = 0
-          isBig = false
-        },
-        biggify(time) {
-          this.scale = vec2(2)
-          CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
-          timer = time
-          isBig = true
+        getSword() {
         }
       }
   }
 
+  function sword() {
+      return {
+
+      }
+  }
+
   const player = add([
-    sprite("mario"), solid(),
+    sprite("player"), solid(),
     //player position
-    pos(30, 0),
+    pos(50, 350),
     //adding gravity
     body(),
     origin("bot"),
-    big()
+    sword()
   ])
 
-  action("shroom", (shroom) => {
-    shroom.move(50, 0)
-  })
 
-  player.on("headbump", (obj) => {
-    if (obj.is("coin-box")) {
-      gameLevel.spawn("$", obj.gridPos.sub(0, 1))
+  player.collides("hookChest", (obj) => {
+      gameLevel.spawn("!", obj.gridPos.sub(0, 2))
       destroy(obj)
-      gameLevel.spawn("}", obj.gridPos.sub(0, 0))
-    }
-    if (obj.is("shroom-box")) {
-      gameLevel.spawn("#", obj.gridPos.sub(0, 1))
+      gameLevel.spawn("+", obj.gridPos.sub(0, 0))
+    })
+  player.collides("hook", (obj) => {
       destroy(obj)
-      gameLevel.spawn("}", obj.gridPos.sub(0, 0))
-    }
-  })
-
-  player.collides("shroom", (shroom) => {
-    destroy(shroom)
-    player.biggify(6)
-  })
-
-  player.collides("coin", (coin) => {
-    destroy(coin)
-    scoreLabel.value++
-    scoreLabel.text = scoreLabel.value
-  })
+      hasHook = true
+    })
+    
 
   action("danger", (danger) => {
     danger.move(-ENEMY_SPEED, 0)
   })
 
   player.collides("danger", (danger) => {
-    if(isJumping) {
-      destroy(danger)
-    } else {
-      go("lose", {score: scoreLabel.value})
-    }
+    go("lose", {score: scoreLabel.value})
   })
-  player.collides("pipe", () => {
-    keyPress("down", () => {
+  player.collides("exit", () => {
       go("game", {
         level: (level + 1),
         score: scoreLabel.value
       })
-    })
   })
   
   player.action(() => {
@@ -191,14 +181,22 @@ const scoreLabel = add([
       go("lose", {score: scoreLabel.value })
     }
   })
+  const successRight = () => {
+    player.move(20, -1500)
+  }
+  const successLeft = () => {
+    player.move(20, -1500)
+  }
 
   //key bindings
 
   keyDown("left", () => {
     player.move(-MOVE_SPEED, 0)
+    direction = 'left'
   })
   keyDown("right", () => {
     player.move(MOVE_SPEED, 0)
+    direction = 'right'
   })
 
   player.action(() => {
@@ -210,7 +208,45 @@ const scoreLabel = add([
   keyPress("space", () => {
     if (player.grounded()) {
       isJumping = true
-      player.jump(CURRENT_JUMP_FORCE)
+      player.jump(JUMP_FORCE)
+    }
+  })
+  keyPress("0", () => {
+    if (hasHook) {
+      if (direction === "right") {
+        const useRope = add([
+          sprite("rope"),
+          rotate(4),
+          pos(player.pos.add(0, -20)),
+          scale(.25),
+          origin("right"),
+          {
+              speed: 100
+          },
+      ]);
+      useRope.collides("grapple", () => {
+        successRight()
+      })
+      setTimeout(() => {destroy(useRope)}, 100);
+      }
+      if (direction === "left") {
+        const useRope = add([
+          sprite("rope"),
+          rotate(150),
+          pos(player.pos.add(0, -20)),
+          scale(.25),
+          origin("right"),
+          {
+              speed: 100
+          },
+      ]);
+      useRope.collides("grapple", () => {
+        successLeft()
+      })
+      setTimeout(() => {destroy(useRope)}, 100);
+      }
+    }else{
+      return
     }
   })
 
