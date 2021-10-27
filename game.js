ec6ff74ad1bd9d96944e188c26faaac9e530bb02
markm5 <mark.mckenna@wrapmate.com>
2021-10-27 11:47:00 -0600

    adding collision methods to coins and shroom

--- game.js
+++ game.js
@@ -7,12 +7,12 @@ kaboom({
 })
 
 loadRoot("https://i.imgur.com/")
-loadSprite("coin", 'wbKxHcd.png')
+loadSprite("coin", 'wbKxhcd.png')
 loadSprite("goomba", "KPO3fR9.png")
 loadSprite("brick", "pogC9x5.png")
 loadSprite("block", "M6rwarW.png")
 loadSprite("mario", "Wb1qfhK.png")
-loadSprite("shroom", "0WMd92p.png")
+loadSprite("shroom", "0wMd92p.png")
 loadSprite("boxClosed", "gesQ1KP.png")
 loadSprite("boxOpened", "bdrLpi6.png")
 loadSprite("pipeTopLeft", "ReTPiWY.png")
@@ -31,7 +31,6 @@ scene("game", () => {
     "         %   =*=%=                                      ",
     "                                                        ",
     "                                                        ",
-    "                                                        ",
     "                                  -+                    ",
     "                           ^  ^   ()                    ",
     "====================================   ================="
@@ -43,14 +42,14 @@ scene("game", () => {
     "=": [sprite("block"), solid()],
     "$": [sprite("coin")],
     "%": [sprite("boxClosed"), solid(), "coin-box"],
-    "*": [sprite("boxClosed"), solid(), "mushroom-box"],
+    "*": [sprite("boxClosed"), solid(), "shroom-box"],
     "}": [sprite("boxOpened"), solid()],
     "(": [sprite("pipeBotLeft"), solid(), scale(0.5)],
     ")": [sprite("pipeBotRight"), solid(), scale(0.5)],
     "-": [sprite("pipeTopLeft"), solid(), scale(0.5)],
     "+": [sprite("pipeTopRight"), solid(), scale(0.5)],
     "^": [sprite("goomba"), solid()],
-    "#": [sprite("shroom"), solid()],
+    "#": [sprite("shroom"), solid(), "shroom", body()],
   }
 
   const gameLevel = addLevel(map, levelConfig)
@@ -65,19 +64,80 @@ const scoreLabel = add([
 
   add([text("level " + "test", pos(4,6))])
 
+  function big() {
+    let timer = 0;
+    let isBig = false
+      return {
+        update() {
+          if (isBig) {
+            timer -= dt()
+            if (timer <= 0) {
+              this.smallify()
+            }
+          }
+        },
+        isBig() {
+          return isBig
+        },
+        smallify() {
+          this.scale = vec2(1)
+          timer = 0
+          isBig = false
+        },
+        biggify(time) {
+          this.scale = vec2(2)
+          timer = time
+          isBig = true
+        }
+      }
+  }
+
   const player = add([
     sprite("mario"), solid(),
     //player position
     pos(30, 0),
     //adding gravity
     body(),
-    origin("bot")
+    origin("bot"),
+    // big()
   ])
+
+  action("shroom", (shroom) => {
+    shroom.move(50, 0)
+  })
+
+  player.on("headbump", (obj) => {
+    if (obj.is("coin-box")) {
+      gameLevel.spawn("$", obj.gridPos.sub(0, 1))
+      destroy(obj)
+      gameLevel.spawn("}", obj.gridPos.sub(0, 0))
+    }
+    if (obj.is("shroom-box")) {
+      gameLevel.spawn("#", obj.gridPos.sub(0, 1))
+      destroy(obj)
+      gameLevel.spawn("}", obj.gridPos.sub(0, 0))
+    }
+  })
+
+  player.collides("shroom", (shroom) => {
+    destroy(shroom)
+    player.biggify(6)
+  })
+
+  player.collides("coin", (coin) => {
+    destroy(coin)
+    scoreLabel.value++
+    scoreLabel.text = scoreLabel.value
+  })
+
   //key bindings
 const MOVE_SPEED = 120;
-const JUMP_FORCE = 10;
+const JUMP_FORCE = 360;
 
   keyDown("left", () => {
+    player.move(-MOVE_SPEED, 0)
+  })
+  keyDown("right", () => {
     player.move(MOVE_SPEED, 0)
   })
 
